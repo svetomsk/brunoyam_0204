83316a4a9810ad3932951fe771ded3aaa47f022f
Gedeburg@ <eugn932008@inbox.ru>
2019-06-03 17:11:46 +0300

    считывание данных

--- src/final_fight/rss_reader/network/INetworkHelper.java
+++ src/final_fight/rss_reader/network/INetworkHelper.java
@@ -1,5 +1,21 @@
 package final_fight.rss_reader.network;
 
-public interface INetworkHelper {
-    String fetchHtml(String url);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class INetworkHelper {
+    public static void main(String[] args) throws IOException {
+        URL address = new URL("https://www.championat.com/rss/news/football/");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(address.openStream())
+        );
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
 }
