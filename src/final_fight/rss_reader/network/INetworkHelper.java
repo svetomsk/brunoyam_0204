ecc554ef2f52baac98f381b3ea2b899053d63f70
Gedeburg@ <eugn932008@inbox.ru>
2019-06-03 22:09:30 +0300

    считывание данных

--- src/final_fight/rss_reader/network/INetworkHelper.java
+++ src/final_fight/rss_reader/network/INetworkHelper.java
@@ -5,17 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class INetworkHelper {
-    public static void main(String[] args) throws IOException {
-        URL address = new URL("https://www.championat.com/rss/news/football/");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(address.openStream())
-        );
+public interface INetworkHelper {
+    class NetworkHelper implements INetworkHelper {
+        public static void main(String[] args) throws IOException {
+            URL address = new URL("https://www.championat.com/rss/news/football/");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(address.openStream())
+            );
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
         }
-        in.close();
     }
 }
+
